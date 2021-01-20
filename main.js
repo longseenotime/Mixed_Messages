8451496b4135287982b0494da42bf8bfd9c3236f
Raymond May <77587339+longseenotime@users.noreply.github.com>
2021-01-20 21:32:10 +0100

    Changed the program to make only inspiring quotes in order to make it easier

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-const jokeHeads = ['Knock, knock ', ];
-const jokeBodies = [];
 const inspiringHeads = [];
 const inspiringBodies = [];
 
 const inspire = () => {
     let head = inspiringHeads[Math.floor(Math.random() * 11)];
     let body = inspiringBodies[Math.floor(Math.random() * 11)];
-
-    return head + body;
-}
-
-const joke = () => {
-    let head = jokeHeads[Math.floor(Math.random() * 11)];
-    let body = jokeBodies[Math.floor(Math.random() * 11)];
 
     return head + body;
 }
