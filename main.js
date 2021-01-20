d839d3a6a94746c59a8cd6bc278de09142f23077
Raymond May <77587339+longseenotime@users.noreply.github.com>
2021-01-20 22:09:13 +0100

    Completed Program just need to test it first

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const inspiringHeads = ['just remember, ', 'even on bad days, ', 'a wise man once said, ', 'never forget, ', 'Rule 1 of being a boss', 'keep in mind'] 
-const inspiringBodies = ['Bros over hoes ', 'cereal goes in the bowl before milk', 'If she still sucking after you nut, she might be a vaccum cleaner', 'I'];
+const inspiringHeads = ['Just remember, ', 'Even on bad days, ', 'A wise man once said, ', 'Never forget, ', 'Rule 1 of being a boss', 'Keep in mind'] 
+const inspiringBodies = ['Bros over hoes ', 'Cereal goes in the bowl before milk', 'If she still sucking after you nut, she might be a vaccum cleaner', "Eppstein didn't kill himself and neither should you", "If you can't do the job, don't give up, just outsource it to a Bangladeshi teenager and pay him 1/4 the minimum wage. Problem solved", "Always keep a pack of gum and bottle of oat milk in you back pocket. No exeptions"];
 const inspiringTails = ['- Barney Stinson', '- Barack Obama. . . Thanks Obama!', '- Mahatma Gandhi', '- Tu Pac', '- Bobi Wine', '- a pimp named Slickback', '- Severus Snape'];
 
 const inspire = () => {
     let head = inspiringHeads[Math.floor(Math.random() * 6)];
     let body = inspiringBodies[Math.floor(Math.random() * 6)];
-
-    return head + body;
+    let tail = inspiringTails[Math.floor(Math.random() * 6)];
+    return head + body + tail;
 }
 
